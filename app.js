f79096b63afca10d3eb1611384428f8b90cad16d
anne barois <barois.anne@orange.fr>
2022-06-29 14:42:48 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello, Express ! 👋'))
+
+app.listen(port, () => console.log(`Notre application Node est démarée sur http://localhost:${port}`))
